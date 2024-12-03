8783c86b91a8b4339b1d7cdb15a29db28bc847a3
Daniel Carrasco Moreno <practicasswap@gmail.com>
2024-12-03 17:40:54 +0000

    incrementalidad a gold done

--- models/marts/core/dim_promos.sql
+++ models/marts/core/dim_promos.sql
@@ -1,3 +1,10 @@
+{{
+  config(
+    materialized='incremental',
+    unique_key = 'promo_id',
+    on_schema_change='fail'
+  )
+}}
     WITH stg_promos AS (
         SELECT *
         FROM {{ref('stg_sql_server_dbo__promos')}}
@@ -15,12 +22,16 @@ dim_promos_gold AS (
         p.status,
         p.discount,
         COUNT(o.order_id) AS total_orders,
-        ROUND(SUM(o.item_order_cost_usd * (p.discount / 100)), 2) as total_discount_given
+        ROUND(SUM(o.item_order_cost_usd * (p.discount / 100)), 2) as total_discount_given,
+        p.DATE_LOAD
     FROM stg_promos p
     LEFT JOIN 
         stg_orders o ON p.promo_id = o.promo_id
+    {% if is_incremental() %}
+        WHERE p.DATE_LOAD > (   SELECT MAX(p.DATE_LOAD) FROM {{ this }} p)
+    {% endif %}
     GROUP BY 
-       p.promo_id, p.status, p.discount
+       p.promo_id, p.status, p.discount, p.DATE_LOAD
 )
 
 SELECT * FROM dim_promos_gold
--- models/marts/core/dim_users.sql
+++ models/marts/core/dim_users.sql
@@ -1,3 +1,10 @@
+{{
+  config(
+    materialized='incremental',
+    unique_key = 'user_id',
+    on_schema_change='fail'
+  )
+}}
     WITH stg_users AS (
         SELECT *
         FROM {{ref('stg_sql_server_dbo__users')}}
@@ -22,11 +29,15 @@ dim_users_gold AS (
         , COUNT(o.order_id) AS total_orders
         ,  ROUND(SUM(o.total_order_cost_usd), 2) AS total_spend
         , MIN(o.created_at_utc) AS first_order_date
+        , u.DATE_LOAD
     FROM stg_users u 
     LEFT JOIN 
         stg_orders o ON u.user_id = o.user_id
+    {% if is_incremental() %}
+	    WHERE u.DATE_LOAD > (   SELECT MAX(u.DATE_LOAD) FROM {{ this }} u)
+    {% endif %}
     GROUP BY 
-        u.user_id, u.first_name, u.last_name, u.email, u.phone_number, u.created_at_utc,u.updated_at_utc, u.address_id
+        u.user_id, u.first_name, u.last_name, u.email, u.phone_number, u.created_at_utc,u.updated_at_utc, u.address_id, u.DATE_LOAD
     )
 
 SELECT * FROM dim_users_gold
